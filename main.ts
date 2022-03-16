ad57fcd2315f6794df9e652c0a0cc92342b1da08
omarturmun <93262369+omarturmun@users.noreply.github.com>
2022-03-16 14:23:17 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,7 @@
+DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 30)
+basic.pause(2000)
+DFRobotMaqueenPlus.mototStop(Motors.ALL)
+basic.pause(500)
+DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CCW, 30)
+basic.pause(2000)
+DFRobotMaqueenPlus.mototStop(Motors.ALL)
